09d9788c9493e2d6679eefe2fd7cef8f399b1cae
Johanna Sörngård <jsorngard@gmail.com>
2024-07-29 14:08:05 +0200

    Add function limit information on new line

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,8 @@ mod sw0;
 mod swm1;
 
 /// -1/e = 0.6065306597126334.
+///
+/// The smallest input value for which the Lambert W functions in this crate return a value.
 pub const NEG_INV_E: f64 = -0.367_879_441_171_442_33;
 
 // 1/sqrt(e)
