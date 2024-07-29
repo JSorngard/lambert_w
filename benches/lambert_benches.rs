7f31b7f717680996ced94e9c27733496f57a1949
Johanna Sörngård <jsorngard@gmail.com>
2024-07-29 21:00:51 +0200

    switch benchmark to smw1

--- benches/lambert_benches.rs
+++ benches/lambert_benches.rs
@@ -1,23 +1,23 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use lambert_w::lambert_w_0;
+use lambert_w::sp_lambert_w_m1;
 use std::hint::black_box;
 
 fn ln_vs_lambert_w_0(c: &mut Criterion) {
     let args = [
-        -2.678794411714424e-01_f64,
-        6.321205588285577e-01,
-        9.632120558828557,
-        9.999632120558828e+03,
-        9.999999996321206e+08,
-        9.999999999999633e+12,
-        1.000000000000000e+18,
-        1.000000000000000e+160,
+        -3.578794411714423e-01_f64,
+        -3.000000000000000e-02,
+        -3.000000000000000e-03,
+        -1.000000000000000e-04,
+        -3.000000000000000e-05,
+        -1.000000000000004e-75,
+        -1.000000000000008e-145,
     ];
 
     for z in args {
-        let mut group = c.benchmark_group(format!("{z}"));
-        group.bench_function(&format!("ln"), |b| b.iter(|| black_box(z.ln())));
-        group.bench_function(&format!("W_0"), |b| b.iter(|| black_box(lambert_w_0(z))));
+        //let mut group = c.benchmark_group(format!("{z}"));
+        c.bench_function(&format!("W_m1 {z}"), |b| {
+            b.iter(|| black_box(sp_lambert_w_m1(z)))
+        });
     }
 }
 
