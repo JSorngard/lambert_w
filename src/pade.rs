e02ed7f0f771841fc67f718cfccb2c1f81466d58
Johanna Sörngård <jsorngard@gmail.com>
2024-07-30 08:13:27 +0200

    Add docstring to pade_3

--- src/pade.rs
+++ src/pade.rs
@@ -1,5 +1,8 @@
 use fast_polynomial::polynomials::poly_3;
 
+/// Third degree Padé approximant. 
+/// The first set of coefficients are for the polynomial in the numerator
+/// and the second set are the coefficients of the polynomial in the denominator.
 pub fn pade_3(x: f64, x2: f64, [n0, n1, n2, n3]: [f64; 4], [d0, d1, d2, d3]: [f64; 4]) -> f64 {
     poly_3(x, x2, n0, n1, n2, n3) / poly_3(x, x2, d0, d1, d2, d3)
 }
