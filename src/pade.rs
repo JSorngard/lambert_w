b34ca186845ce9615709db7f25e09f9a99de975d
Johanna Sörngård <jsorngard@gmail.com>
2024-07-31 12:37:48 +0200

    Ensure pade doesn't leak

--- src/pade.rs
+++ src/pade.rs
@@ -4,7 +4,12 @@ use fast_polynomial::polynomials::{poly_3, poly_4};
 /// The first set of coefficients are for the polynomial in the numerator
 /// and the second set are the coefficients of the polynomial in the denominator.
 #[inline(always)]
-pub fn pade_3(x: f64, x2: f64, [n0, n1, n2, n3]: [f64; 4], [d0, d1, d2, d3]: [f64; 4]) -> f64 {
+pub(crate) fn pade_3(
+    x: f64,
+    x2: f64,
+    [n0, n1, n2, n3]: [f64; 4],
+    [d0, d1, d2, d3]: [f64; 4],
+) -> f64 {
     poly_3(x, x2, n0, n1, n2, n3) / poly_3(x, x2, d0, d1, d2, d3)
 }
 
@@ -12,7 +17,7 @@ pub fn pade_3(x: f64, x2: f64, [n0, n1, n2, n3]: [f64; 4], [d0, d1, d2, d3]: [f6
 /// The first set of coefficients are for the polynomial in the numerator
 /// and the second set are the coefficients of the polynomial in the denominator.
 #[inline(always)]
-pub fn pade_4_3(
+pub(crate) fn pade_4_3(
     x: f64,
     x2: f64,
     x4: f64,
