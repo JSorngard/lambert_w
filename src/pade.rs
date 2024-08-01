08ef79248fd3e441812b4675b7693664d44a2b82
Johanna Sörngård <jsorngard@gmail.com>
2024-08-01 21:36:06 +0200

    Make dw0c linear search

--- src/pade.rs
+++ src/pade.rs
@@ -73,32 +73,34 @@ pub(crate) fn pade_7(
 }
 
 #[cfg(feature = "50bits")]
-/// Padé approximant consisting of an eigth degree polynomial divided by a seventh degree polynomial.
-///
-/// The first set of coefficients are for the polynomial in the numerator
-/// and the second set are the coefficients of the polynomial in the denominator.
-///
-/// If the `fma` feature is enabled this uses Estrin's scheme and fused multiply-add instructions, otherwise it uses the more typical Horner's method.
 #[inline(always)]
-pub(crate) fn pade_8_7(
+pub(crate) fn pade_8_8(
     x: f64,
     [n0, n1, n2, n3, n4, n5, n6, n7, n8]: [f64; 9],
-    [d0, d1, d2, d3, d4, d5, d6, d7]: [f64; 8],
+    [d0, d1, d2, d3, d4, d5, d6, d7, d8]: [f64; 9],
 ) -> f64 {
     #[cfg(feature = "estrin")]
     {
-        use fast_polynomial::polynomials::{poly_7, poly_8};
+        // use fast_polynomial::polynomials::poly_8;
 
-        let x2 = x * x;
-        let x4 = x2 * x2;
-        let x8 = x4 * x4;
-        poly_8(x, x2, x4, x8, n0, n1, n2, n3, n4, n5, n6, n7, n8)
-            / poly_7(x, x2, x4, d0, d1, d2, d3, d4, d5, d6, d7)
+        // let x2 = x * x;
+        // let x4 = x2 * x2;
+        // let x8 = x4 * x4;
+        // poly_8(x, x2, x4, x8, n0, n1, n2, n3, n4, n5, n6, n7, n8)
+        //     / poly_8(x, x2, x4, x8, d0, d1, d2, d3, d4, d5, d6, d7, d8)
+
+        fast_polynomial::rational_array(
+            x,
+            &[n0, n1, n2, n3, n4, n5, n6, n7, n8],
+            &[d0, d1, d2, d3, d4, d5, d6, d7, d8],
+        )
     }
 
     #[cfg(not(feature = "estrin"))]
     {
         (n0 + x * (n1 + x * (n2 + x * (n3 + x * (n4 + x * (n5 + x * (n6 + x * (n7 + x * n8))))))))
-            / (d0 + x * (d1 + x * (d2 + x * (d3 + x * (d4 + x * (d5 + x * (d6 + x * d7)))))))
+            / (d0
+                + x * (d1
+                    + x * (d2 + x * (d3 + x * (d4 + x * (d5 + x * (d6 + x * (d7 + x * d8))))))))
     }
 }
