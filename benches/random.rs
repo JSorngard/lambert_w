304ded2468a250d0d426227fdde402475e7b76d0
Johanna Sörngård <jsorngard@gmail.com>
2024-09-02 06:45:38 +0200

    add random benchmarks for f32 versions

--- benches/random.rs
+++ benches/random.rs
@@ -1,10 +1,14 @@
 use core::f64::consts::E;
 use core::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
-use lambert_w::{lambert_w0, lambert_wm1, sp_lambert_w0, sp_lambert_wm1};
+use lambert_w::{
+    lambert_w0, lambert_w0f, lambert_wm1, lambert_wm1f, sp_lambert_w0, sp_lambert_wm1,
+};
 use rand::{Rng, SeedableRng};
 use rand_pcg::Pcg32;
 use std::time::Instant;
+
+const E32: f32 = E as f32;
 
 fn random_benches(c: &mut Criterion) {
     let mut group = c.benchmark_group("random inputs");
@@ -33,6 +37,18 @@ fn random_benches(c: &mut Criterion) {
             start.elapsed()
         })
     });
+    group.bench_function("W_0 24 bits on f32", |b| {
+        b.iter_custom(|iters| {
+            let datas: Vec<f32> = (0..iters)
+                .map(|_| rng.gen_range(-1.0 / E32..f32::MAX))
+                .collect();
+            let start = Instant::now();
+            for &z in &datas {
+                black_box(lambert_w0f(z));
+            }
+            start.elapsed()
+        })
+    });
     group.bench_function("W_-1 50 bits", |b| {
         b.iter_custom(|iters| {
             let datas: Vec<f64> = (0..iters).map(|_| rng.gen_range(-1.0 / E..=0.0)).collect();
@@ -49,6 +65,18 @@ fn random_benches(c: &mut Criterion) {
             let start = Instant::now();
             for &z in &datas {
                 black_box(sp_lambert_wm1(z));
+            }
+            start.elapsed()
+        })
+    });
+    group.bench_function("W_-1 24 bits on f32", |b| {
+        b.iter_custom(|iters| {
+            let datas: Vec<f32> = (0..iters)
+                .map(|_| rng.gen_range(-1.0 / E32..=0.0))
+                .collect();
+            let start = Instant::now();
+            for &z in &datas {
+                black_box(lambert_wm1f(z));
             }
             start.elapsed()
         })
