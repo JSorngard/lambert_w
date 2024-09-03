784adc5ced090e6eabb9890ca17788f1f90fd392
Johanna Sörngård <jsorngard@gmail.com>
2024-09-03 12:58:15 +0200

    Move the benchmark code itself into the generic function

--- benches/random.rs
+++ benches/random.rs
@@ -1,5 +1,7 @@
-use core::{f32::consts::E as E32, f64::consts::E as E64, ops::RangeBounds, time::Duration};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use core::{f32::consts::E as E32, f64::consts::E as E64, ops::RangeBounds};
+use criterion::{
+    black_box, criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion,
+};
 use lambert_w::{
     lambert_w0, lambert_w0f, lambert_wm1, lambert_wm1f, sp_lambert_w0, sp_lambert_wm1,
 };
@@ -10,55 +12,83 @@ use rand::{
 };
 use std::time::Instant;
 
-fn init_vec_of_rng_and_bench<R, T, F>(f: F, range: R, iters: u64, rng: &mut SmallRng) -> Duration
-where
+/// Generates a vec of random values in the given range and benchmarks the given function
+/// on those values.
+///
+/// It generates the random values before starting the time measurement in the benchmark,
+/// and drops them after finishing, such that the generation and drop have no effect on the measured time.
+fn bench_on_vec_of_random_values_in_range<'a, R, T, F, Prng>(
+    group: &mut BenchmarkGroup<'a, WallTime>,
+    id: &str,
+    f: F,
+    range: R,
+    rng: &mut Prng,
+) where
     R: RangeBounds<T> + Clone + SampleRange<T>,
     T: SampleUniform + Copy + PartialOrd,
     F: Fn(T) -> T,
+    Prng: Rng,
 {
-    let datas: Vec<T> = (0..iters).map(|_| rng.gen_range(range.clone())).collect();
-    let start = Instant::now();
-    datas.iter().for_each(|&z| {
-        black_box(f(z));
+    group.bench_function(id, |b| {
+        b.iter_custom(|iters| {
+            let datas: Vec<T> = (0..iters).map(|_| rng.gen_range(range.clone())).collect();
+            let start = Instant::now();
+            for &z in &datas {
+                black_box(f(z));
+            }
+            let duration = start.elapsed();
+            drop(datas);
+            duration
+        })
     });
-    let duration = start.elapsed();
-    drop(datas);
-    duration
 }
 
 fn random_benches(c: &mut Criterion) {
     let mut group = c.benchmark_group("random inputs");
     let mut rng = SmallRng::seed_from_u64(0b1010101010101);
-    group.bench_function("W_0 50 bits", |b| {
-        b.iter_custom(|iters| {
-            init_vec_of_rng_and_bench(lambert_w0, -1.0 / E64..f64::MAX, iters, &mut rng)
-        })
-    });
-    group.bench_function("W_0 24 bits", |b| {
-        b.iter_custom(|iters| {
-            init_vec_of_rng_and_bench(sp_lambert_w0, -1.0 / E64..f64::MAX, iters, &mut rng)
-        })
-    });
-    group.bench_function("W_0 24 bits on f32", |b| {
-        b.iter_custom(|iters| {
-            init_vec_of_rng_and_bench(lambert_w0f, -1.0 / E32..f32::MAX, iters, &mut rng)
-        })
-    });
-    group.bench_function("W_-1 50 bits", |b| {
-        b.iter_custom(|iters| {
-            init_vec_of_rng_and_bench(lambert_wm1, -1.0 / E64..=0.0, iters, &mut rng)
-        })
-    });
-    group.bench_function("W_-1 24 bits", |b| {
-        b.iter_custom(|iters| {
-            init_vec_of_rng_and_bench(sp_lambert_wm1, -1.0 / E64..=0.0, iters, &mut rng)
-        })
-    });
-    group.bench_function("W_-1 24 bits on f32", |b| {
-        b.iter_custom(|iters| {
-            init_vec_of_rng_and_bench(lambert_wm1f, -1.0 / E32..=0.0, iters, &mut rng)
-        })
-    });
+
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
+        "W_0 50 bits",
+        lambert_w0,
+        -1.0 / E64..f64::MAX,
+        &mut rng,
+    );
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
+        "W_0 24 bits",
+        sp_lambert_w0,
+        -1.0 / E64..f64::MAX,
+        &mut rng,
+    );
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
+        "W_0 24 bits on f32",
+        lambert_w0f,
+        -1.0 / E32..f32::MAX,
+        &mut rng,
+    );
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
+        "W_-1 50 bits",
+        lambert_wm1,
+        -1.0 / E64..=0.0,
+        &mut rng,
+    );
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
+        "W_-1 24 bits",
+        sp_lambert_wm1,
+        -1.0 / E64..=0.0,
+        &mut rng,
+    );
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
+        "W_-1 24 bits on f32",
+        lambert_wm1f,
+        -1.0 / E32..=0.0,
+        &mut rng,
+    );
 }
 
 criterion_group!(benches, random_benches);
