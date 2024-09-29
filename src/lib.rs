53c227a3380954f7789ebbcf5864832156fcfc42
Johanna Sörngård <jsorngard@gmail.com>
2024-09-29 15:29:41 +0200

    Add tests (that fail right now)

--- src/lib.rs
+++ src/lib.rs
@@ -345,39 +345,43 @@ mod r#trait {
 
     pub trait LambertW0: Float {
         /// The principal branch of the Lambert W funciton.
-        fn lambert_w0(&self) -> Self;
+        fn lambert_w0(self) -> Self;
     }
 
     pub trait LambertWm1: Float {
         /// The secondary branch of the Lambert W funciton.
-        fn lambert_wm1(&self) -> Self;
+        fn lambert_wm1(self) -> Self;
     }
 
     #[cfg(feature = "24bits")]
     impl LambertW0 for f32 {
-        fn lambert_w0(&self) -> Self {
-            lambert_w0f(*self)
+        #[inline]
+        fn lambert_w0(self) -> Self {
+            lambert_w0f(self)
         }
     }
 
     #[cfg(feature = "24bits")]
     impl LambertWm1 for f32 {
-        fn lambert_wm1(&self) -> Self {
-            lambert_wm1f(*self)
+        #[inline]
+        fn lambert_wm1(self) -> Self {
+            lambert_wm1f(self)
         }
     }
 
     #[cfg(feature = "50bits")]
     impl LambertW0 for f64 {
-        fn lambert_w0(&self) -> Self {
-            lambert_w0(*self)
+        #[inline]
+        fn lambert_w0(self) -> Self {
+            lambert_w0(self)
         }
     }
 
     #[cfg(feature = "50bits")]
     impl LambertWm1 for f64 {
-        fn lambert_wm1(&self) -> Self {
-            lambert_wm1(*self)
+        #[inline]
+        fn lambert_wm1(self) -> Self {
+            lambert_wm1(self)
         }
     }
 }
@@ -833,5 +837,44 @@ mod test {
         #[cfg(feature = "estrin")]
         assert_abs_diff_eq!(lambert_wm1f(-1e-20), -49.962_986, epsilon = 1e-5);
         assert!(lambert_wm1f(f32::EPSILON).is_nan());
+    }
+
+    #[cfg(all(feature = "trait", feature = "50bits"))]
+    #[test]
+    fn test_lambert_w0_trait_50() {
+        use super::LambertW0;
+        assert_abs_diff_eq!(
+            -2.678_794_411_714_424e-1_f64.lambert_w0(),
+            -3.993_824_525_397_807e-1
+        );
+    }
+
+    #[cfg(all(feature = "trait", feature = "24bits"))]
+    #[test]
+    fn test_lambert_w0_trait_24() {
+        use super::LambertW0;
+        assert_abs_diff_eq!(6.321_205_5e-1_f32.lambert_w0(), 4.167_04e-1, epsilon = 1e-7);
+    }
+
+    #[cfg(all(feature = "trait", feature = "50bits"))]
+    #[test]
+    fn test_lambert_wm1_trait_50() {
+        use super::LambertWm1;
+        assert_abs_diff_eq!(
+            -3.578_794_411_714_423e-1_f64.lambert_wm1(),
+            -1.253493791367214,
+            epsilon = 1e-14
+        );
+    }
+
+    #[cfg(all(feature = "trait", feature = "24bits"))]
+    #[test]
+    fn test_lambert_wm1_trait_24() {
+        use super::LambertWm1;
+        assert_abs_diff_eq!(
+            -3.578_794_3e-1_f32.lambert_wm1(),
+            -1.253_493_8,
+            epsilon = 1e-6
+        );
     }
 }
