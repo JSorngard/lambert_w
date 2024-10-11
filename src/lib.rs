abfa30522931d88fb7a9940b13ab2cbd390995f8
Johanna Sörngård <jsorngard@gmail.com>
2024-10-11 15:30:10 +0200

    More information in rounding motivation

--- src/lib.rs
+++ src/lib.rs
@@ -125,10 +125,11 @@ const INV_SQRT_E: f64 = 0.606_530_659_712_633_4;
 /// Fulfills the equation Ωe^Ω = 1.
 ///
 /// Has been rounded to the closest available `f64` value.
-// If we round the last digits (87) to a 9 then the closest available float
-// is further away from the true value than if we round to 8.
+// If we round the last two digits (87) to 9 rustc sets the constant to
+//                     0.567_143_290_409_784
 //        Rounded from 0.567_143_290_409_783_87
 pub const OMEGA: f64 = 0.567_143_290_409_783_8;
+
 
 /// The principal branch of the Lambert W function computed to 24 bits of accuracy on `f64`s.
 ///
