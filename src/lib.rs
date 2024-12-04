6613d1d59dcdbacbb1489a53eee4475929806784
Johanna Sörngård <jsorngard@gmail.com>
2024-12-04 19:39:55 +0100

    `cargo fmt`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! # lambert_w
-//! 
+//!
 //! Fast and accurate evaluation of the real valued parts of the principal and secondary branches of the [Lambert W function](https://en.wikipedia.org/wiki/Lambert_W_function)
 //! with the method of Toshio Fukushima \[[1](#references)\].
 //!
@@ -101,7 +101,7 @@
 //! **Precise and fast computation of Lambert W function by piecewise minimax rational function approximation with variable transformation**.
 //! DOI: [10.13140/RG.2.2.30264.37128](https://doi.org/10.13140/RG.2.2.30264.37128).
 //! November 2020.
-//! 
+//!
 //! [⬆️ Back to top](#lambert_w).
 
 #![cfg_attr(not(feature = "std"), no_std)]
