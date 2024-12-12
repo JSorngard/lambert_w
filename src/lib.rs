e902bd8d17163810fce49c80c733cdb108c9e2f5
Johanna <jsorngard@gmail.com>
2024-12-12 13:45:49 +0100

    Method description tweaks

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 //! Fast and accurate evaluation of the real valued parts of the principal and secondary branches of the [Lambert W function](https://en.wikipedia.org/wiki/Lambert_W_function)
 //! with the method of Toshio Fukushima \[[1](#references)\].
 //!
-//! This method uses no recursion, loops, memory allocation, or any kind of iterative procedure
-//! other than what might already exist in the logarithm, square root, or division implementations
-//! on your platform.  
+//! This method is not an iterative procedure, it does not allocate memory and
+//! uses no recursion or loops, other than what might already exist in the logarithm,
+//! square root, or division implementations on your platform.  
 //! Instead it works by splitting the domain of the function into subdomains,
 //! and on each subdomain it uses a rational function evaluated on a simple transformation of
 //! the input to describe the function.  
