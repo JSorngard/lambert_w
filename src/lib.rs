0fd11fa3a76b9c80f58bc6250fbac2df0432bad7
Johanna Sörngård <jsorngard@gmail.com>
2024-12-13 12:37:16 +0100

    Title docs with overview instead of repeating crate name

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! # lambert_w
+//! # Overview
 //!
 //! Fast and accurate evaluation of the real valued parts of the principal and secondary branches of the [Lambert W function](https://en.wikipedia.org/wiki/Lambert_W_function)
 //! with the method of Toshio Fukushima \[[1](#references)\].
@@ -108,7 +108,7 @@
 //! DOI: [10.13140/RG.2.2.30264.37128](https://doi.org/10.13140/RG.2.2.30264.37128).
 //! November 2020.
 //!
-//! [⬆️ Back to top](#lambert_w).
+//! [⬆️ Back to top](#overview).
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![forbid(unsafe_code)]
