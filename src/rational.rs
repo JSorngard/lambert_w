f716cb52221a5f3135e204894899beb3685642c2
Johanna Sörngård <jsorngard@gmail.com>
2024-12-30 19:59:09 +0100

    Retry the `estrin` feature, but only when the tests pass.

--- src/rational.rs
+++ src/rational.rs
@@ -8,7 +8,17 @@
 /// and the second set are the coefficients of the polynomial in the denominator.
 #[inline(always)]
 pub fn rational_3_over_3(x: f64, [n0, n1, n2, n3]: [f64; 4], [d0, d1, d2, d3]: [f64; 4]) -> f64 {
-    (n0 + x * (n1 + x * (n2 + x * n3))) / (d0 + x * (d1 + x * (d2 + x * d3)))
+    #[cfg(feature = "estrin")]
+    {
+        let x2 = x * x;
+        fast_polynomial::polynomials::poly_3(x, x2, n0, n1, n2, n3)
+            / fast_polynomial::polynomials::poly_3(x, x2, d0, d1, d2, d3)
+    }
+
+    #[cfg(not(feature = "estrin"))]
+    {
+        (n0 + x * (n1 + x * (n2 + x * n3))) / (d0 + x * (d1 + x * (d2 + x * d3)))
+    }
 }
 
 /// Rational function consisting of two third degree polynomials.
@@ -30,7 +40,17 @@ pub fn rational_4_over_3(
     [n0, n1, n2, n3, n4]: [f64; 5],
     [d0, d1, d2, d3]: [f64; 4],
 ) -> f64 {
-    (n0 + x * (n1 + x * (n2 + x * (n3 + x * n4)))) / (d0 + x * (d1 + x * (d2 + x * d3)))
+    #[cfg(feature = "estrin")]
+    {
+        let x2 = x * x;
+        fast_polynomial::polynomials::poly_4(x, x2, x2 * x2, n0, n1, n2, n3, n4)
+            / fast_polynomial::polynomials::poly_3(x, x2, d0, d1, d2, d3)
+    }
+
+    #[cfg(not(feature = "estrin"))]
+    {
+        (n0 + x * (n1 + x * (n2 + x * (n3 + x * n4)))) / (d0 + x * (d1 + x * (d2 + x * d3)))
+    }
 }
 
 /// Rational function consisting of a fourth degree polynomial divided by a third degree polynomial.
@@ -43,7 +63,17 @@ pub fn rational_4_over_3f(
     [n0, n1, n2, n3, n4]: [f32; 5],
     [d0, d1, d2, d3]: [f32; 4],
 ) -> f32 {
-    (n0 + x * (n1 + x * (n2 + x * (n3 + x * n4)))) / (d0 + x * (d1 + x * (d2 + x * d3)))
+    #[cfg(feature = "estrin")]
+    {
+        let x2 = x * x;
+        fast_polynomial::polynomials::poly_4(x, x2, x2 * x2, n0, n1, n2, n3, n4)
+            / fast_polynomial::polynomials::poly_3(x, x2, d0, d1, d2, d3)
+    }
+
+    #[cfg(not(feature = "estrin"))]
+    {
+        (n0 + x * (n1 + x * (n2 + x * (n3 + x * n4)))) / (d0 + x * (d1 + x * (d2 + x * d3)))
+    }
 }
 
 /// Rational function consisting of two seventh degree polynomials.
@@ -70,8 +100,19 @@ pub fn rational_8_over_7(
     [n0, n1, n2, n3, n4, n5, n6, n7, n8]: [f64; 9],
     [d0, d1, d2, d3, d4, d5, d6, d7]: [f64; 8],
 ) -> f64 {
-    (n0 + x * (n1 + x * (n2 + x * (n3 + x * (n4 + x * (n5 + x * (n6 + x * (n7 + x * n8))))))))
-        / (d0 + x * (d1 + x * (d2 + x * (d3 + x * (d4 + x * (d5 + x * (d6 + x * d7)))))))
+    #[cfg(feature = "estrin")]
+    {
+        let x2 = x * x;
+        let x4 = x2 * x2;
+        fast_polynomial::polynomials::poly_8(x, x2, x4, x4 * x4, n0, n1, n2, n3, n4, n5, n6, n7, n8)
+            / fast_polynomial::polynomials::poly_7(x, x2, x4, d0, d1, d2, d3, d4, d5, d6, d7)
+    }
+
+    #[cfg(not(feature = "estrin"))]
+    {
+        (n0 + x * (n1 + x * (n2 + x * (n3 + x * (n4 + x * (n5 + x * (n6 + x * (n7 + x * n8))))))))
+            / (d0 + x * (d1 + x * (d2 + x * (d3 + x * (d4 + x * (d5 + x * (d6 + x * d7)))))))
+    }
 }
 
 #[cfg(test)]
