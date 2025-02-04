f4b803f8af9c5bccf6dff4d875c4d7159bb40648
Johanna Sörngård <jsorngard@gmail.com>
2025-02-04 14:33:40 +0100

    Make the build script more modular and add comments

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
 use std::env;
 
 fn main() {
+    /// The name of the environment variable that can be set to enable the check for panics.
     // If you change this, remember to also change the environment variable
-    // that is set in the CI configuration.
+    // that is set in the CI job that checks for panics.
     const ENV_KEY: &str = "LAMBERT_W_ENSURE_NO_PANICS";
-    // The suggestion to the user if the profile is not set to `release-lto`.
-    const SUGGESTION: &str =
-        "The \"release-lto\" profile must be enabled to ensure no false positives.";
+
+    /// The value we look for in the environment variable to enable the check for panics.
+    const ENV_VAL: &str = "1";
+
+    /// The profile needed to ensure no false positives in the check for panics.
+    const NEEDED_PROFILE: &str = "release-lto";
+
+    // Changes to the above constants need to be mentioned in the CHANGELOG.md file.
 
     // Re-run the build script if the environment variable changes.
     println!("cargo:rerun-if-env-changed={ENV_KEY}");
@@ -15,24 +21,29 @@ fn main() {
     println!("cargo:rustc-check-cfg=cfg(assert_no_panic)");
 
     if let Ok(env_val) = env::var(ENV_KEY) {
-        // If the environment variable at `ENV_KEY` is set to 1 we use the `no-panic` crate to attempt to verify that the crate can not panic.
-        if env_val == "1" {
+        // If the environment variable at `ENV_KEY` is set to `ENV_VAL` we use the `no-panic` crate to attempt to verify that the crate can not panic.
+        if env_val == ENV_VAL {
             // Enable the `assert_no_panic` cfg option.
             println!("cargo:rustc-cfg=assert_no_panic");
 
-            // This requires the `release-lto` profile to be enabled, otherwise it will result in false positives.
+            // The suggestion to the user if the profile is not set to the needed profile.
+            let suggestion = format!(
+                "The \"{NEEDED_PROFILE}\" profile must be enabled to ensure no false positives."
+            );
+
+            // This requires a specific profile to be enabled, otherwise it will result in false positives.
             // We emit a compilation warning if we can not determine that this profile is enabled.
             match parse_build_profile_name_from_environment() {
                 Ok(Some(profile_name)) => {
-                    if profile_name != "release-lto" {
-                        println!("cargo:warning=the `{ENV_KEY}` environment variable is set to 1, but a profile that could result in false positives seems to be enabled. {SUGGESTION}");
+                    if profile_name != NEEDED_PROFILE {
+                        println!("cargo:warning=the `{ENV_KEY}` environment variable is set to {ENV_VAL}, but a profile that could result in false positives seems to be enabled. {suggestion}");
                     }
                 }
                 Ok(None) => {
-                    println!("cargo:warning=the `{ENV_KEY}` environment variable is set to 1, but the build profile name could not be determined. {SUGGESTION}");
+                    println!("cargo:warning=the `{ENV_KEY}` environment variable is set to {ENV_VAL}, but the build profile name could not be determined. {suggestion}");
                 }
                 Err(e) => {
-                    println!("cargo:warning=the `{ENV_KEY}` environment variable is set to 1, but the `OUT_DIR` environment variable could not be read due to: {e}\nThe profile could therefore not be determined. {SUGGESTION}");
+                    println!("cargo:warning=the `{ENV_KEY}` environment variable is set to {ENV_VAL}, but the `OUT_DIR` environment variable could not be read due to: {e}\nThe profile could therefore not be determined. {suggestion}");
                 }
             }
         }
