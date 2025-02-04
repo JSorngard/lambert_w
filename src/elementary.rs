a71f05b6e5c40d7f9ce8e0ac69c7f6fc369a8895
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-02-04 07:43:43 +0100

    Motivate why the panics can never happen

--- src/elementary.rs
+++ src/elementary.rs
@@ -17,8 +17,15 @@ pub fn sqrt(x: f64) -> f64 {
         libm::sqrt(x)
     }
 
-    #[cfg(all(not(feature = "std"), not(feature = "libm")))]
+    #[cfg(not(any(feature = "std"), feature = "libm"))]
     {
+        // This panic can never be triggered
+        // since it's a compile error to not 
+        // have at least one of the `std` and 
+        // `libm` features enabled.
+        // This panic code will therefore 
+        // never actually be inserted into 
+        // any binary.
         panic!("computing sqrt({x}) needs at least one of the `std` or `libm` feature flags to be enabled");
     }
 }
@@ -35,7 +42,7 @@ pub fn sqrtf(x: f32) -> f32 {
         libm::sqrtf(x)
     }
 
-    #[cfg(all(not(feature = "std"), not(feature = "libm")))]
+    #[cfg(not(any(feature = "std"), feature = "libm"))]
     {
         panic!("computing sqrtf({x}) needs at least one of the `std` or `libm` feature flags to be enabled");
     }
