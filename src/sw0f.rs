95f35054130c06c3cff798b1c411a90ab4b79b07
Johanna Sörngård <jsorngard@gmail.com>
2025-02-05 12:58:51 +0100

    Move the panic to the normal execution path

--- src/sw0f.rs
+++ src/sw0f.rs
@@ -13,12 +13,8 @@ const NEG_INV_E: f32 = super::NEG_INV_E as f32;
 
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn sw0f(z: f32) -> f32 {
-    if z == -10.0 {
-        panic!("at the disco");
-    }
-
     if z < NEG_INV_E || z.is_nan() {
-        f32::NAN
+        panic!();
     } else if z <= 2.008_217_8 {
         // W <= 0.854, X_1
 
