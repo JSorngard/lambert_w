676aed6c0d52d4ce8dba2e63b69893d7941848b0
Johanna Sörngård <jsorngard@gmail.com>
2025-02-15 23:30:03 +0100

    inline the internal function into the public ones instead

--- src/lib.rs
+++ src/lib.rs
@@ -70,7 +70,6 @@ pub const OMEGA: f64 = 0.567_143_290_409_783_8;
 // the `assert_no_panic` attribute if that variable is set.
 // We then check for that attribute when testing and if so we statically ensure that no
 // function in the crate can panic using the `no-panic` crate.
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn sp_lambert_w0(z: f64) -> f64 {
     sw0::sw0(z)
@@ -102,7 +101,6 @@ pub fn sp_lambert_w0(z: f64) -> f64 {
 /// # Reference
 ///
 /// [Toshio Fukushima, Precise and fast computation of Lambert W function by piecewise minimax rational function approximation with variable transformation](https://www.researchgate.net/publication/346309410_Precise_and_fast_computation_of_Lambert_W_function_by_piecewise_minimax_rational_function_approximation_with_variable_transformation).
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn sp_lambert_wm1(z: f64) -> f64 {
     swm1::swm1(z)
@@ -133,7 +131,6 @@ pub fn sp_lambert_wm1(z: f64) -> f64 {
 /// # Reference
 ///
 /// [Toshio Fukushima, Precise and fast computation of Lambert W function by piecewise minimax rational function approximation with variable transformation](https://www.researchgate.net/publication/346309410_Precise_and_fast_computation_of_Lambert_W_function_by_piecewise_minimax_rational_function_approximation_with_variable_transformation).
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn lambert_w0(z: f64) -> f64 {
     dw0c::dw0c(z - NEG_INV_E)
@@ -168,7 +165,6 @@ pub fn lambert_w0(z: f64) -> f64 {
 /// # Reference
 ///
 /// [Toshio Fukushima, Precise and fast computation of Lambert W function by piecewise minimax rational function approximation with variable transformation](https://www.researchgate.net/publication/346309410_Precise_and_fast_computation_of_Lambert_W_function_by_piecewise_minimax_rational_function_approximation_with_variable_transformation).
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn lambert_w0f(z: f32) -> f32 {
     sw0f::sw0f(z)
@@ -200,7 +196,6 @@ pub fn lambert_w0f(z: f32) -> f32 {
 /// # Reference
 ///
 /// [Toshio Fukushima, Precise and fast computation of Lambert W function by piecewise minimax rational function approximation with variable transformation](https://www.researchgate.net/publication/346309410_Precise_and_fast_computation_of_Lambert_W_function_by_piecewise_minimax_rational_function_approximation_with_variable_transformation).
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn lambert_wm1(z: f64) -> f64 {
     dwm1c::dwm1c(z, z - NEG_INV_E)
@@ -236,7 +231,6 @@ pub fn lambert_wm1(z: f64) -> f64 {
 /// # Reference
 ///
 /// [Toshio Fukushima, Precise and fast computation of Lambert W function by piecewise minimax rational function approximation with variable transformation](https://www.researchgate.net/publication/346309410_Precise_and_fast_computation_of_Lambert_W_function_by_piecewise_minimax_rational_function_approximation_with_variable_transformation).
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn lambert_wm1f(z: f32) -> f32 {
     swm1f::swm1f(z)
