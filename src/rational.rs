234a7eaa7395c2e945a31f5f9ca7777645074cd1
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-02-16 12:44:17 +0100

    Add more info to doc comment

--- src/rational.rs
+++ src/rational.rs
@@ -21,6 +21,9 @@ impl Zero for f64 {
 }
 
 /// Evaluate a rational function at `x` using Horner's method.
+///
+/// The coeffieients are sorted by the degree of their corresponding `x` term,
+/// in ascending order.
 #[inline(always)]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 pub fn rational_function<T, const N: usize, const D: usize>(
