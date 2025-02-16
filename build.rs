aa3de00be88a95e71da2e4b0078d2f933c15ce65
Johanna Sörngård <jsorngard@gmail.com>
2025-02-16 12:31:52 +0100

    cargo fmt

--- build.rs
+++ build.rs
@@ -31,8 +31,9 @@ fn main() {
         // Enable the `assert_no_panic` cfg option.
         println!("cargo:rustc-cfg=assert_no_panic");
 
-        let suggestion =
-            format!("The `{NEEDED_PROFILE}` build profile must be enabled to ensure no false positives.");
+        let suggestion = format!(
+            "The `{NEEDED_PROFILE}` build profile must be enabled to ensure no false positives."
+        );
 
         // In order for `no-panic` to not cause false positives fat LTO needs to be enabled.
         // This crate defines a profile for that.
