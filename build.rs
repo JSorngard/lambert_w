9a1ac64f99f00c8dfb8d3276f9d438a95d915ffa
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-02-27 09:54:11 +0100

    Specify `as_str` as a closure instead of an associated method

--- build.rs
+++ build.rs
@@ -27,7 +27,7 @@ fn main() {
 
     let env_val = env::var(ENV_KEY);
 
-    if env_val.as_ref().map(String::as_str) == Ok(ENV_VAL) {
+    if env_val.as_ref().map(|s| s.as_str()) == Ok(ENV_VAL) {
         // Enable the `assert_no_panic` cfg option.
         println!("cargo:rustc-cfg=assert_no_panic");
 
