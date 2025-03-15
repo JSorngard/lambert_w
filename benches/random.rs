0b0f0beeb342bf26b943e11b90aa79982179dad4
Johanna Sörngård <jsorngard@gmail.com>
2025-03-15 14:43:35 +0100

    Add Halley iteration function to benchamrks

--- benches/random.rs
+++ benches/random.rs
@@ -1,11 +1,10 @@
-use core::f32::consts::E as E32;
-use core::f64::consts::E as E64;
 use core::ops::RangeBounds;
 use criterion::{
     black_box, criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion,
 };
 use lambert_w::{
-    lambert_w0, lambert_w0f, lambert_wm1, lambert_wm1f, sp_lambert_w0, sp_lambert_wm1,
+    lambert_w0, lambert_w0f, lambert_wk, lambert_wm1, lambert_wm1f, sp_lambert_w0, sp_lambert_wm1,
+    NEG_INV_E,
 };
 use rand::{
     distr::uniform::{SampleRange, SampleUniform},
@@ -19,7 +18,7 @@ use std::time::Instant;
 ///
 /// It generates the random values before starting the time measurement in the benchmark,
 /// and drops them after finishing, such that the generation and drop have no effect on the measured time.
-fn bench_on_vec_of_random_values_in_range<'a, R, T, F, Prng>(
+fn bench_on_vec_of_random_values_in_range<'a, R, T, F, U, Prng>(
     group: &mut BenchmarkGroup<'a, WallTime>,
     id: &str,
     f: F,
@@ -28,7 +27,7 @@ fn bench_on_vec_of_random_values_in_range<'a, R, T, F, Prng>(
 ) where
     R: Clone + RangeBounds<T> + SampleRange<T>,
     T: Copy + PartialOrd + SampleUniform,
-    F: Fn(T) -> T,
+    F: Fn(T) -> U,
     Prng: Rng,
 {
     group.bench_function(id, |b| {
@@ -53,9 +52,17 @@ fn random_benches(c: &mut Criterion) {
 
     bench_on_vec_of_random_values_in_range(
         &mut group,
+        "W_0 Halley iteration",
+        |z| lambert_wk(0, z.into()),
+        NEG_INV_E..=f64::MAX,
+        &mut rng,
+    );
+
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
         "W_0 50 bits",
         lambert_w0,
-        -1.0 / E64..f64::MAX,
+        NEG_INV_E..f64::MAX,
         &mut rng,
     );
 
@@ -63,7 +70,7 @@ fn random_benches(c: &mut Criterion) {
         &mut group,
         "W_0 24 bits",
         sp_lambert_w0,
-        -1.0 / E64..f64::MAX,
+        NEG_INV_E..f64::MAX,
         &mut rng,
     );
 
@@ -71,7 +78,15 @@ fn random_benches(c: &mut Criterion) {
         &mut group,
         "W_0 24 bits on f32",
         lambert_w0f,
-        -1.0 / E32..f32::MAX,
+        NEG_INV_E as f32..f32::MAX,
+        &mut rng,
+    );
+
+    bench_on_vec_of_random_values_in_range(
+        &mut group,
+        "W_-1 Halley iteration",
+        |z| lambert_wk(-1, z.into()),
+        NEG_INV_E..=0.0,
         &mut rng,
     );
 
@@ -79,7 +94,7 @@ fn random_benches(c: &mut Criterion) {
         &mut group,
         "W_-1 50 bits",
         lambert_wm1,
-        -1.0 / E64..=0.0,
+        NEG_INV_E..=0.0,
         &mut rng,
     );
 
@@ -87,7 +102,7 @@ fn random_benches(c: &mut Criterion) {
         &mut group,
         "W_-1 24 bits",
         sp_lambert_wm1,
-        -1.0 / E64..=0.0,
+        NEG_INV_E..=0.0,
         &mut rng,
     );
 
@@ -95,7 +110,7 @@ fn random_benches(c: &mut Criterion) {
         &mut group,
         "W_-1 24 bits on f32",
         lambert_wm1f,
-        -1.0 / E32..=0.0,
+        NEG_INV_E as f32..=0.0,
         &mut rng,
     );
 }
