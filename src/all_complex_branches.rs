353813828a85cfa9bdd18fc54a2b6291e07e21f6
Johanna Sörngård <jsorngard@gmail.com>
2025-03-23 09:51:13 +0100

    generic helpers

--- src/all_complex_branches.rs
+++ src/all_complex_branches.rs
@@ -2,10 +2,9 @@
 //! This implementation is capable of computing the function at any point in the complex plane on any branch.
 
 use num_complex::{Complex, Complex32, Complex64, ComplexFloat};
-use num_traits::{Float, FromPrimitive, Signed};
+use num_traits::{Float, FromPrimitive, Signed, Zero};
 
 use core::{
-    f32::NEG_INFINITY,
     f64::consts::{E, PI},
     ops::{Add, Mul, Sub, SubAssign},
 };
@@ -21,7 +20,7 @@ use crate::NEG_INV_E;
 /// Close to the branch cut at -1/e this function may be slightly less accurate.
 ///
 /// If you know you want the principal or secondary branches where they are real valued,
-/// take a look at the [`lambert_w0`](crate::lambert_w0) or [`lambert_wm1`](crate::lambert_wm1) functions and their 32-bit equivalents instead.
+/// take a look at the [`lambert_w0`](crate::lambert_w0) or [`lambert_wm1`](crate::lambert_wm1) functions instead.
 /// They can be up to two orders of magnitude faster.
 ///
 /// # Examples
@@ -61,6 +60,48 @@ pub fn lambert_w(k: i32, z_re: f64, z_im: f64) -> (f64, f64) {
     (w.re, w.im)
 }
 
+/// Computes branch `k` of the complex valued Lambert W function
+/// using Halley's method.
+///
+/// The return value is a tuple where the first element is the
+/// real part and the second element is the imaginary part.
+///
+/// Close to the branch cut at -1/e this function may be slightly less accurate.
+///
+/// If you know you want the principal or secondary branches where they are real valued,
+/// take a look at the [`lambert_w0f`](crate::lambert_w0f) or [`lambert_wm1f`](crate::lambert_wm1f) functions instead.
+/// They can be up to two orders of magnitude faster.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// use lambert_w::lambert_wf;
+///
+/// // W_2(1 + 2i)
+/// let w = lambert_wf(2, 1.0, 2.0);
+///
+/// assert_eq!(w, (-1.6869138, 11.962631));
+/// ```
+///
+/// Returns [`NAN`](f32::NAN)s if any of the inputs are infinite:
+///
+/// ```
+/// # use lambert_w::lambert_wf;
+/// let w = lambert_wf(-13, f32::INFINITY, 0.0);
+///
+/// assert!(w.0.is_nan() && w.1.is_nan());
+/// ```
+///
+/// or `NAN`:
+///
+/// ```
+/// # use lambert_w::lambert_wf;
+/// let w = lambert_wf(1_000, 0.0, f32::NAN);
+///
+/// assert!(w.0.is_nan() && w.1.is_nan());
+/// ```
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
 #[must_use = "this is a pure function that only returns a value and has no side effects"]
 pub fn lambert_wf(k: i16, z_re: f32, z_im: f32) -> (f32, f32) {
@@ -86,7 +127,7 @@ where
     const MAX_ITER: usize = 30;
     /// If the absolute difference between two consecutive iterations is less than this value,
     /// the iteration stops.
-    const PREC: f32 = 1e-30;
+    const PREC: f64 = 1e-30;
 
     let i_zero = U::zero();
     let i_one = U::one();
@@ -96,8 +137,8 @@ where
     let d_one = T::one();
     let d_neg_one = -d_one;
     let d_two = d_one + d_one;
-    let d_e = T::from_f64(E).unwrap_or(T::from_f32(E as f32).unwrap());
-    let d_pi = T::from_f64(PI).unwrap_or(T::from_f32(PI as f32).unwrap());
+    let d_e: T = t_from_f64_or_f32(E);
+    let d_pi: T = t_from_f64_or_f32(PI);
 
     let i = Complex::<T>::i();
     let z_zero = Complex::<T>::from(d_zero);
@@ -106,13 +147,12 @@ where
     let z_two = z_one + z_one;
 
     let z_e = Complex::<T>::from(d_e);
-    let z_neg_inv_e = Complex::<T>::from(
-        T::from_f64(NEG_INV_E).unwrap_or(T::from_f32(NEG_INFINITY as f32).unwrap()),
-    );
+    let z_neg_inv_e = complex_t_from_f64_or_f32(NEG_INV_E);
     let z_half = z_one / z_two;
 
     let abs_one = z_one.abs();
     let abs_half = z_half.abs();
+    let abs_prec = complex_t_from_f64_or_f32(PREC).abs();
 
     // region: special cases
 
@@ -140,12 +180,8 @@ where
     // Choose the initial point more carefully when we are close to the branch cut.
     if (z - z_neg_inv_e).abs() <= abs_one {
         let p = (d_two * (d_e * z + d_one)).sqrt();
-        let p2 = z_neg_one + p - z_one / Complex::<T>::from(T::from_f32(3.0).unwrap()) * p * p;
-        let p3 = Complex::<T>::from(T::from_f32(11.0).unwrap())
-            / Complex::<T>::from(T::from_f32(72.0).unwrap())
-            * p
-            * p
-            * p;
+        let p2 = z_neg_one + p - z_one / t_from_f64_or_f32(3.0) * p * p;
+        let p3 = complex_t_from_f64_or_f32(11.0) / complex_t_from_f64_or_f32(72.0) * p * p * p;
         if k == i_zero {
             w = p2 + p3;
         } else if (k == i_one && z.im < d_zero) || (k == i_neg_one && z.im > d_zero) {
@@ -155,19 +191,20 @@ where
 
     if k == i_zero && (z - z_half).abs() <= abs_half {
         // Order (1,1) Padé approximant for the principal branch
-        w = (T::from_f32(0.35173371).unwrap()
-            * (T::from_f32(0.1237166).unwrap() + T::from_f32(7.061302897).unwrap() * z))
-            / (d_two + T::from_f32(0.827184).unwrap() * (d_one + d_two * z));
+        w = (t_from_f64_or_f32::<T>(0.35173371)
+            * (t_from_f64_or_f32::<T>(0.1237166) + t_from_f64_or_f32::<T>(7.061302897) * z))
+            / (d_two + t_from_f64_or_f32::<T>(0.827184) * (d_one + d_two * z));
     }
 
     if k == i_neg_one && (z - z_half).abs() <= abs_half {
         // Order (1,1) Padé approximant for the secondary branch
-        w = -(((T::from_f32(2.2591588985).unwrap() + T::from_f32(4.22096).unwrap() * i)
-            * ((T::from_f32(-14.073271).unwrap() - T::from_f32(33.767687754).unwrap() * i) * z
-                - (T::from_f32(12.7127).unwrap() - T::from_f32(19.071643).unwrap() * i)
+        w = -(((t_from_f64_or_f32::<T>(2.2591588985) + t_from_f64_or_f32::<T>(4.22096) * i)
+            * ((t_from_f64_or_f32::<T>(-14.073271) - t_from_f64_or_f32::<T>(33.767687754) * i)
+                * z
+                - (t_from_f64_or_f32::<T>(12.7127) - t_from_f64_or_f32::<T>(19.071643) * i)
                     * (d_one + d_two * z)))
             / (d_two
-                - (T::from_f32(17.23103).unwrap() - T::from_f32(10.629721).unwrap() * i)
+                - (t_from_f64_or_f32::<T>(17.23103) - t_from_f64_or_f32::<T>(10.629721) * i)
                     * (d_one + d_two * z)));
     }
 
@@ -186,12 +223,29 @@ where
 
         iter += 1;
 
-        if (w - w_prev).abs() <= Complex::<T>::from(T::from_f32(PREC).unwrap()).abs()
-            || iter >= MAX_ITER
-        {
+        if (w - w_prev).abs() <= abs_prec || iter >= MAX_ITER {
             return w;
         }
     }
 
     // endregion: Halley iteration
+}
+
+/// Attempts to convert a `f64` to a `T`. If that fails, it tries to convert the `f64` to a `f32` and then to a `T`.
+///
+/// # Panics
+///
+/// Panics if a `T` cannot be created from a `f32`.
+fn t_from_f64_or_f32<T>(x: f64) -> T
+where
+    T: FromPrimitive,
+{
+    T::from_f64(x).unwrap_or(T::from_f32(x as f32).unwrap())
+}
+
+fn complex_t_from_f64_or_f32<T>(x: f64) -> Complex<T>
+where
+    T: FromPrimitive + Zero,
+{
+    Complex::<T>::new(t_from_f64_or_f32(x), T::zero())
 }
