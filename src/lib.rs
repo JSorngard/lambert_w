1b14f19e01615e4c5f215bf35d718ce4b9e931b9
Johanna Sörngård <jsorngard@gmail.com>
2025-04-27 09:03:11 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // Copyright 2024 Johanna Sörngård
-// SPDX-License-Identifier: MIT OR Apache-2.0 
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
 #![doc = include_str!("../README.md")]
 #![no_std]
