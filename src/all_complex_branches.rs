62270ed25e5ae83c8a2378f225ad64726e320ae6
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-04-27 08:55:16 +0200

    Update all_complex_branches.rs

--- src/all_complex_branches.rs
+++ src/all_complex_branches.rs
@@ -1,3 +1,5 @@
+// Copyright 2024 Johanna Sörngård
+// SPDX-License-Identifier: MIT OR Apache-2.0
 //! This module contains the general implementation of the Lambert W function.
 //! This implementation is capable of computing the function at any point in the complex plane on any branch.
 
