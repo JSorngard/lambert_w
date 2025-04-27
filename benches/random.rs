3be4d0e0534f871320a0285e7022634f87380da0
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2025-04-27 08:57:20 +0200

    Update random.rs

--- benches/random.rs
+++ benches/random.rs
@@ -1,3 +1,5 @@
+// Copyright 2024 Johanna Sörngård
+// SPDX-License-Identifier: MIT OR Apache-2.0
 use core::ops::RangeBounds;
 use criterion::{
     black_box, criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion,
