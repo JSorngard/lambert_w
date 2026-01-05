af47a8e588db67bb6ce6099ddcee4f296ff7e1be
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-01-05 21:48:29 +0100

    Update copyright year in generic_math.rs

--- src/generic_math.rs
+++ src/generic_math.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 Johanna Sörngård
+// Copyright 2026 Johanna Sörngård
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 //! This module contains elementary and rational functions used in the Lambert W function approximations.
