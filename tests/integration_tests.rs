1894d0402fe55cdfd3abca83f1000e99231140f2
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-01-19 19:23:09 +0100

    Update integration_tests.rs

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -589,7 +589,7 @@ fn test_iterative_version() {
     assert!(lambert_w(0, f64::NAN, f64::NAN, err_tol).0.is_nan());
     assert!(lambert_w(0, f64::NAN, f64::NAN, err_tol).1.is_nan());
     assert!(lambert_w(0, 0.0, 0.0, f64::NAN).0.is_nan());
-    assert!(lambert_w(0, 0.0, 0.0, f64::NAN).1.is_nan()); 
+    assert!(lambert_w(0, 0.0, 0.0, f64::NAN).1.is_nan());
     // Infinity
     assert!(lambert_w(0, f64::INFINITY, 0.0, err_tol).0.is_nan());
     assert!(lambert_w(0, f64::INFINITY, 0.0, err_tol).1.is_nan());
