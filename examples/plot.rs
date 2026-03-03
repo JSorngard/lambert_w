09ba3a7791f186c886d116d9c6f4aca71e20c4f6
Johanna Sörngård <jsorngard@gmail.com>
2026-03-03 22:34:21 +0100

    Use `kuva` instead of `plotters`.

--- examples/plot.rs
+++ examples/plot.rs
@@ -1,61 +1,53 @@
-// Copyright 2024-2026 Johanna Sörngård
+// Copyright 2026 Johanna Sörngård
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 //! This example is intended to be ran more than studied.
 //! It generates a plot of the two branches of the function
 //! and saves it as a png file.
 
+use kuva::{
+    backend::svg::SvgBackend,
+    plot::LinePlot,
+    render::{layout::Layout, plots::Plot, render::render_multiple},
+};
 use lambert_w::{lambert_w0, lambert_wm1, NEG_INV_E};
-use plotters::prelude::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("lambert_w_plot.png", (1920, 1080)).into_drawing_area();
+fn main() {
+    const X_MAX: f64 = 5.0;
+    const X_MIN: f64 = NEG_INV_E;
+    const NUM_PLOT_POINTS: u32 = 1000;
 
-    root.fill(&WHITE)?;
-    let root = root.margin(10, 10, 10, 10);
+    let x_coords: Vec<f64> = (0..NUM_PLOT_POINTS)
+        // Map to the range [0, 1].
+        .map(|i| f64::from(i) / f64::from(NUM_PLOT_POINTS - 1))
+        // Map to the range [X_MIN, X_MAX].
+        .map(|t| t * (X_MAX - X_MIN) + X_MIN)
+        .collect();
 
-    let mut chart = ChartBuilder::on(&root)
-        .caption("Lambert W function", ("sans-serif", 40).into_font())
-        .set_all_label_area_size(20)
-        .top_x_label_area_size(0)
-        .right_y_label_area_size(0)
-        .build_cartesian_2d(-1.0..10.0, -4.0..2.0)?;
+    let plots = vec![
+        Plot::Line(
+            LinePlot::new()
+                .with_data(x_coords.iter().map(|&x| (x, lambert_w0(x))))
+                .with_color("black")
+                .with_stroke_width(3.0)
+                .with_legend("Principal branch, k=0"),
+        ),
+        Plot::Line(
+            LinePlot::new()
+                .with_data(x_coords.into_iter().map(|x| (x, lambert_wm1(x))))
+                .with_color("red")
+                .with_stroke_width(3.0)
+                .with_legend("Secondary branch, k=-1"),
+        ),
+    ];
 
-    chart.configure_mesh().x_labels(20).y_labels(20).draw()?;
+    let layout = Layout::auto_from_plots(&plots)
+        .with_title("Lambert W function")
+        .with_x_label("x")
+        .with_y_label("y");
 
-    let steps: u32 = 10000;
-    chart
-        .draw_series(LineSeries::new(
-            (0..steps)
-                // Convert step index into number between 0 and 1.
-                .map(|x| f64::from(x) / f64::from(steps))
-                // Map it to the x-axis.
-                .map(|t| t * (10.0 - NEG_INV_E) + NEG_INV_E)
-                // Apply the principal branch of the Lambert W function
-                // to the x-value.
-                .map(|x| (x, lambert_w0(x))),
-            BLACK,
-        ))?
-        .label("W_0(x)")
-        .legend(|(x, y)| Rectangle::new([(x - 5, y), (x + 10, y)], BLACK));
+    let scene = render_multiple(plots, layout);
+    let svg = SvgBackend.render_scene(&scene);
 
-    chart
-        .draw_series(LineSeries::new(
-            (0..steps)
-                .map(|x| f64::from(x) / f64::from(steps))
-                .map(|t| NEG_INV_E - t * (NEG_INV_E + 0.073))
-                .map(|x| (x, lambert_wm1(x))),
-            RED,
-        ))?
-        .label("W_-1(x)")
-        .legend(|(x, y)| Rectangle::new([(x - 5, y), (x + 10, y)], RED));
-
-    chart
-        .configure_series_labels()
-        .background_style(BLACK.mix(0.1))
-        .border_style(BLACK)
-        .draw()?;
-
-    root.present()?;
-    Ok(())
+    std::fs::write("lambert_w_plot.svg", &svg).unwrap();
 }
