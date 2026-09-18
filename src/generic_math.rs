77634259ba6878ba5bada98f5ec39bc787e495da
Johanna Sörngård <44257381+JSorngard@users.noreply.github.com>
2026-09-18 12:57:09 +0200

    Update generic_math.rs

--- src/generic_math.rs
+++ src/generic_math.rs
@@ -44,7 +44,7 @@ fn polynomial<T: Float, const N: usize>(x: T, coefficients: [T; N]) -> T {
         .copied()
         .rev()
         .skip(1)
-        .fold(coefficients.last().unwrap_or(0), |acc, c| acc * x + c)
+        .fold(*coefficients.last().unwrap_or(&T::zero()), |acc, c| acc * x + c)
 }
 
 // The functions below are wrappers around the [`num-traits`] crate,
